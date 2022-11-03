66c19bbd0a6671e9636c3fb307bf4d7c5bca58c4
Ericmfon <113532062+Ericmfon@users.noreply.github.com>
2022-11-03 11:34:40 -0300

    Initial commit

---
+++ canvas.js
@@ -0,0 +1,40 @@
+var canvas = document.getElementById("mycanvas");
+var context = canvas.getContext("2d");
+context.beginPath();
+
+context.fillStyle = 'rgb(255, 255, 255)';
+context.fillRect(325, 140, 130, 320);
+
+context.fillStyle ='rgb(255, 255, 255)';
+context.fillRect(230, 240, 320, 130);
+
+
+var canvas = document.getElementById("seccanvas");
+var context = canvas.getContext("2d");
+context.beginPath();
+
+context.fillStyle = 'rgb(255, 255, 255)';
+context.fillRect(215, 0, 140, 600);
+
+context.fillStyle ='rgb(255, 255, 255)';
+context.fillRect(0, 230, 800, 140);
+
+context.fillStyle = 'rgb(0, 32, 91)';
+context.fillRect(250, 0, 70, 600);
+
+context.fillStyle ='rgb(0, 32, 91)';
+context.fillRect(0, 265, 800, 70);
+
+
+
+var canvas = document.getElementById("thicanvas");
+var context = canvas.getContext("2d");
+context.beginPath();
+
+context.fillStyle = 'rgb(0, 0, 0)';
+context.fillRect(0, 0, 800, 200);
+
+context.fillStyle ='rgb(255, 204, 0)';
+context.fillRect(0, 400, 800, 200);
+
+
